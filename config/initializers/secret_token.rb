5697decdd0f1b4ff3fd0df58e43791710e2e3e80
sbrocos <sergiobrocos@gmail.com>
2013-05-17 19:52:14 +0200

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teamsb::Application.config.secret_token = 'fd486dcf6b56cb1f478b930dcf1eaa759b05131ce856e15b92dc15a132358c47f30114821a98df92cbf6f64b8b73cd0617a975572274bc3e9221aeadbf5c524e'
