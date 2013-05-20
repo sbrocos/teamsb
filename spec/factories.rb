3bbdef0496f085285e46bb70416a31533db69ce5
sbrocos <sergiobrocos@gmail.com>
2013-05-20 16:42:59 +0200

    refine factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,4 +5,13 @@ FactoryGirl.define do
     password              "foobar"
     password_confirmation "foobar"
   end
-end
+  factory :admin do
+    admin true
+  end
+
+  factory :app do
+    name  "Lorem ipsim"
+    description_short "Lorem ipsum dolor sit amet, consectetuer adipiscing elit."
+    description_large "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat."
+  end
+end                                                                                                                                                                                                   "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat."
