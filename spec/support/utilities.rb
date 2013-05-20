f21280b3302b105958b99335e8e56f55d5bca621
sbrocos <sergiobrocos@gmail.com>
2013-05-20 12:47:24 +0200

    action edit/update user information

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -2,3 +2,11 @@ def full_title(page_title)
   base_title = "teamsb"
   page_title.empty? ? base_title : "#{base_title} | #{page_title}"
 end
+
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",      with: user.mail
+  fill_in "Password",   with: user.password
+  click_button "Sign in"
+  cookies[:remember_token] = user.remember_token
+end
