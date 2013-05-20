cb80e6fb011d3aedcbb2ca0de94e91f931fa9869
sbrocos <sergiobrocos@gmail.com>
2013-05-20 11:55:22 +0200

    changes

--- spec/requests/authenticate_pages_spec.rb
+++ spec/requests/authenticate_pages_spec.rb
@@ -8,23 +8,7 @@ describe "Authentication" do
 
     it { should have_selector('h1', text: 'Sign in') }
     it { should have_selector('title', text: full_title('Sign in')) }
-    it { should have_selector('div.alert.alert-error', text: 'Invalid') }
-
-    describe "with valid information" do
-      let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email
-        fill_in "Password", with: user.password
-        click button "Sign in"
-      end
-
-      it { should have_selector('title', text: user.name) }
-      it { should have_link('Profile', text: user_path(user)) }
-      it { should have_link('Sign out', text: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) }
-    end
   end
-
   describe "signin" do
     before { visit signin_path }
 
@@ -39,7 +23,27 @@ describe "Authentication" do
         it { should_not have_selector('div.alert.alert-error') }
       end
     end
+
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        fill_in "Email",    with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button "Sign in"
+      end
+
+      it { should have_selector('title', text: user.name) }
+
+      it { should have_link('Users',    href: users_path) }
+      it { should have_link('Profile',  href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
+      it { should have_link('Sign out', href: signout_path) }
+      it { should_not have_link('Sign in', href: signin_path) }
+
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
+    end
   end
-
-
 end
