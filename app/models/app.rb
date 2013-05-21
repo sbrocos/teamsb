86e3248fcb0bd5ee44c19fd02fca5108196e194c
sbrocos <sergiobrocos@gmail.com>
2013-05-21 19:48:37 +0200

    change views

--- app/models/app.rb
+++ app/models/app.rb
@@ -2,7 +2,7 @@ class App < ActiveRecord::Base
   attr_accessible :description_large, :description_short, :name
   has_and_belongs_to_many  :users
 
-  validates :name, presence: true
-  validates :description_short, presence: true
+  validates :name, presence: true, :length => { maximum: 50}
+  validates :description_short, presence: true, :length => { maximum: 100}
   validates :description_large, presence: true
 end
