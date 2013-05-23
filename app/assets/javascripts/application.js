755f32307d2207b629bef35b23d66585d28441c9
sbrocos <sergiobrocos@gmail.com>
2013-05-23 10:56:33 +0200

    prepare for start with backbone: backbone:install

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require underscore
+//= require backbone
+//= require teamsb
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
 //= require_tree .
 
