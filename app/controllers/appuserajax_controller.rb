742febf27b25246ff950bf6b5e3887d09c29ace4
sbrocos <sergiobrocos@gmail.com>
2013-05-23 19:32:38 +0200

    change

--- app/controllers/appuserajax_controller.rb
+++ app/controllers/appuserajax_controller.rb
@@ -10,25 +10,17 @@ class AppuserajaxController < ApplicationController
   def create
     @id = params[:app_id]
 
-    rel = Appuser.find_by_app_id(@id)
-
-    if rel
-      respond_with false
+    if params[:action] == 'delete'
+      Appuser.find_by_app_id(@id).destroy
+      respond_with @app = App.find(@id)
     else
-      @app = App.find(@id)
-      @app.appusers.create(user_id: current_user.id)
-      respond_with @app
-    end
-  end
-
-  def destroy
-    @id = params[:app_id]
-    rel = Appuser.find_by_app_id(@id)
-    if !rel
-      respond_with false
-    else
-      @app = App.find(@id)
-      @app.appusers.destroy_all
+      rel = Appuser.find_by_app_id(@id)
+      if rel
+        respond_with false
+      else
+        @app = App.find(@id)
+        @app.appusers.create(user_id: current_user.id)
+      end
       respond_with @app
     end
   end
